42a6fc13176479c045390c9d7475daebe4e9833c
zhuzivq <141465993+zhuzivq@users.noreply.github.com>
2023-12-16 21:05:22 +0800

    Update mtk-cmdq-helper.c

--- drivers/soc/mediatek/mtk-cmdq-helper.c
+++ drivers/soc/mediatek/mtk-cmdq-helper.c
@@ -593,10 +593,9 @@ void *cmdq_pkt_get_curr_buf_va(struct cmdq_pkt *pkt)
 {
 	struct cmdq_pkt_buffer *buf;
 
-	if (unlikely(!pkt->avail_buf_size))
-		if (cmdq_pkt_add_cmd_buffer(pkt) < 0)
+	/* if (unlikely(!pkt->avail_buf_size))
+		if (cmdq_pkt_add_cmd_buffer(pkt) < 0) */
 			/* return -ENOMEM; */
-			return;
 
 	buf = list_last_entry(&pkt->buf, typeof(*buf), list_entry);
 
